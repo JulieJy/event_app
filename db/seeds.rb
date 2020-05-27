9f0fc0eb1bd32a664312d65c7f4baa6ba3d0a9ca
Julie-kiwi <jaumary.julie@gmail.com>
2020-05-27 18:43:31 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+p 'I destroy all events'
+Event.destroy_all
+
+p 'I create 10 upcoming events'
+i = 0
+10.times do
+  event = Event.new(
+    name: Faker::Music::RockBand.name,
+    date: Faker::Date.between(from: Date.today, to: 20.days.from_now),
+    email: Faker::Internet.email,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  )
+  event.save
+  i += 1
+end
+
+p 'I create 10 past events'
+10.times do
+  event = Event.new(
+    name: Faker::Music::RockBand.name,
+    date: Faker::Date.between(from: 20.days.ago, to: Date.today),
+    email: Faker::Internet.email,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+  )
+  event.save
+  i += 1
+end
+
+p 'Finished !'
+
+# p 'I destroy all ingredients'
+# Ingredient.destroy_all
+
+# p 'I create 10 new ingredients'
+# 10.times do
+#   Ingredient.create(
+#     name: Faker::Food.ingredient
+#     )
+# end
+# p 'Finished !'
