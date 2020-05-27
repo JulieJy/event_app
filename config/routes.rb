b1533647d5f872c2789ed33dd0ca5f6a9ea0a212
Julie-kiwi <jaumary.julie@gmail.com>
2020-05-27 18:42:41 +0200

    routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: "events#index"
+  resources :events
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
